e973c231e0308633ee4d32b4fbe63c364b93b769
ketan <ketanpardhi04@gmail.com>
2023-12-25 17:35:51 +0530

    all users page

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,5 +2,35 @@
     <x-slot name="header">
         {{ __('All Users')}}
     </x-slot>
-    This is users section content
+
+    <div class="overflow-x-auto border rounded-lg border-gray-300 dark:border-gray-600">
+        <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-600">
+            <thead class="bg-gradient-to-r from-teal-600 to-teal-800 text-white">
+                <tr>
+                    <th class="py-3 px-4 text-left">ID</th>
+                    <th class="py-3 px-4 text-left">Name</th>
+                    <th class="py-3 px-4 text-left">Email</th>
+                    <th class="py-3 px-4 text-left">Role</th>
+                    <th class="py-3 px-4 text-left">Active</th>
+                    <th class="py-3 px-4 text-left">Created At</th>
+                    <th class="py-3 px-4 text-left">Updated At</th>
+                </tr>
+            </thead>
+            <tbody class="text-gray-700 dark:text-gray-200">
+                @if($users)
+                    @foreach ($users as $user)
+                        <tr class="hover:bg-gray-100 dark:hover:bg-gray-800">
+                            <td class="py-2 px-4">{{ $user->id }}</td>
+                            <td class="py-2 px-4">{{ $user->name }}</td>
+                            <td class="py-2 px-4">{{ $user->email }}</td>
+                            <td class="py-2 px-4">{{ $user->role->name }}</td>
+                            <td class="py-2 px-4">{{ $user->active == 1 ? 'Yes' : 'No'}}</td>
+                            <td class="py-2 px-4">{{ $user->created_at->diffForHumans() }}</td>
+                            <td class="py-2 px-4">{{ $user->updated_at->diffForHumans() }}</td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+    </div>
 </x-admin-layout>
